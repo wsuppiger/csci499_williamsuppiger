6426faf58f18f8ff559d436f96079cb972fd4063
williamsuppiger <suppiger@usc.edu>
2021-02-10 00:33:26 +0000

    added lock to avoid race conditions

--- src/key_value.cc
+++ src/key_value.cc
@@ -6,27 +6,32 @@
 namespace backend {
 
 void KeyValue::Put(const std::string& key, const std::string& value) {
-  std::unordered_map<std::string, std::vector<std::string>>::iterator
-      storage_iter = storage_.find(key);
+  lock_.lock();
+  auto storage_iter = storage_.find(key);
   if (storage_iter != storage_.end()) {
     storage_iter->second.push_back(value);
   } else {
-    std::vector<std::string> value_list {value};
+    std::vector<std::string> value_list{value};
     storage_[key] = value_list;
   }
+  lock_.unlock();
 }
 
-std::vector<std::string> KeyValue::Get(const std::string& key) const {
-  std::unordered_map<std::string, std::vector<std::string>>::const_iterator
-      storage_iter = storage_.find(key);
+std::vector<std::string> KeyValue::Get(const std::string& key) {
+  lock_.lock();
+  auto storage_iter = storage_.find(key);
   if (storage_iter != storage_.end()) {
+    lock_.unlock();
     return storage_iter->second;
   }
+  lock_.unlock();
   return {};
 }
 
 void KeyValue::Remove(const std::string& remove) {
+  lock_.lock();
   storage_.erase(remove);
+  lock_.unlock();
 }
 
 }  // namespace backend
--- src/key_value.h
+++ src/key_value.h
@@ -4,6 +4,7 @@
 #ifndef SRC_KEY_VALUE_H_
 #define SRC_KEY_VALUE_H_
 
+#include <mutex>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -13,17 +14,19 @@ namespace backend {
 // key value storage for backend application
 class KeyValue {
  public:
-  KeyValue() : storage_() {}
+  KeyValue() : storage_(), lock_() {}
 
   virtual ~KeyValue() {}
 
   void Put(const std::string& key, const std::string& value);
-  std::vector<std::string> Get(const std::string& key) const;
+  std::vector<std::string> Get(const std::string& key);
   void Remove(const std::string& remove);
 
  private:
   // stores key value pairs in map of vectors
   std::unordered_map<std::string, std::vector<std::string>> storage_;
+  // lock to prevent race conditions on storage access
+  std::mutex lock_;
 };
 }  // namespace backend
 
