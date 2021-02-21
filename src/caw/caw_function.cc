385dba939e88b099fc5d005d12ae71af4892f95c
williamsuppiger <suppiger@usc.edu>
2021-02-21 05:45:35 +0000

    added caw functions for caw related reading and writing

--- src/caw/caw_function.cc
+++ src/caw/caw_function.cc
@@ -4,6 +4,8 @@
 #include "caw_function.h"
 
 #include <glog/logging.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 #include <vector>
 
@@ -13,7 +15,7 @@ namespace csci499 {
 std::unordered_map<std::string,
                    std::function<CawFuncReply(const Any&, KeyValueInterface&)> >
     CawFunction::function_map_ = {{"registeruser", &CawFunction::RegisterUser},
-                                  {"caw", &CawFunction::Caw},
+                                  {"caw", &CawFunction::CawCreate},
                                   {"follow", &CawFunction::Follow},
                                   {"read", &CawFunction::Read},
                                   {"profile", &CawFunction::Profile}};
@@ -36,9 +38,43 @@ CawFuncReply CawFunction::RegisterUser(const Any& payload,
   return {Status(StatusCode::OK, "successfully registered user " + username)};
 }
 
-CawFuncReply CawFunction::Caw(const Any& payload, KeyValueInterface& kv) {
-  // wait for prof email
-  return {Status::OK};
+CawFuncReply CawFunction::CawCreate(const Any& payload, KeyValueInterface& kv) {
+  CawRequest request;
+  payload.UnpackTo(&request);
+  std::string username = request.username();
+  std::string text = request.text();
+  std::string parent_id = request.parent_id();
+  std::vector<std::string> prev_caws = kv.Get("caws");
+  // the caw id for the current caw is the previous count of all caws before it
+  std::string caw_id = std::to_string(prev_caws.size());
+  if (!UserExists(username, kv)) {
+    LOG(WARNING) << "the user does not exist";
+    return {Status(StatusCode::UNAVAILABLE, "the user does not exist")};
+  } else if (!parent_id.empty()) {
+    if (std::find(prev_caws.begin(), prev_caws.end(), parent_id) ==
+        prev_caws.end()) {
+      LOG(WARNING) << "the parent caw does not exist";
+      return {Status(StatusCode::UNAVAILABLE, "the parent caw does not exist")};
+    } else {
+      // store all children ids of a Caw in a key cawchildren-<parent-id>
+      kv.Put("cawchildren-" + parent_id, caw_id);
+    }
+  }
+  kv.Put("caws", caw_id);
+  timeval curr_time;
+  gettimeofday(&curr_time, NULL);
+  Timestamp time;
+  time.set_seconds(curr_time.tv_sec);
+  time.set_useconds(curr_time.tv_usec);
+  Caw caw;  // create caw message
+  caw.set_username(username);
+  caw.set_text(text);
+  caw.set_id(caw_id);
+  caw.set_parent_id(parent_id);
+  caw.mutable_timestamp()->CopyFrom(time);
+  // store serialized caw in key caw-<caw-id>
+  kv.Put("caw-" + caw_id, caw.SerializeAsString());
+  return {Status::OK, caw.SerializeAsString()};
 }
 
 CawFuncReply CawFunction::Follow(const Any& payload, KeyValueInterface& kv) {
@@ -71,8 +107,23 @@ CawFuncReply CawFunction::Follow(const Any& payload, KeyValueInterface& kv) {
 }
 
 CawFuncReply CawFunction::Read(const Any& payload, KeyValueInterface& kv) {
-  // wait for prof email
-  return {Status::OK};
+  ReadRequest request;
+  payload.UnpackTo(&request);
+  std::string caw_id = request.caw_id();
+  std::vector<std::string> caws = kv.Get("caws");
+  if (std::find(caws.begin(), caws.end(), caw_id) == caws.end()) {
+    LOG(WARNING) << "the caw does not exist";
+    return {Status(StatusCode::UNAVAILABLE, "the caw does not exist")};
+  }
+  std::vector<Caw> caw_replys;
+  // recursively find all children/replys to caw_id
+  ReadReplys(caw_id, kv, caw_replys);
+  ReadReply reply;
+  for (int i = 0; i < caw_replys.size(); ++i) {
+    Caw* c = reply.add_caws();
+    c->CopyFrom(caw_replys[i]);
+  }
+  return {Status::OK, reply.SerializeAsString()};
 }
 
 CawFuncReply CawFunction::Profile(const Any& payload, KeyValueInterface& kv) {
@@ -86,10 +137,10 @@ CawFuncReply CawFunction::Profile(const Any& payload, KeyValueInterface& kv) {
   std::vector<std::string> followers = kv.Get("ufollowers-" + username);
   std::vector<std::string> following = kv.Get("ufollowing-" + username);
   ProfileReply reply;
-  for (auto f : followers) {
+  for (const auto& f : followers) {
     reply.add_followers(f);
   }
-  for (auto f : following) {
+  for (const auto& f : following) {
     reply.add_following(f);
   }
   return {Status::OK, reply.SerializeAsString()};
@@ -99,6 +150,17 @@ bool CawFunction::UserExists(const std::string& username,
                              KeyValueInterface& kv) {
   std::vector<std::string> users = kv.Get("users");
   return std::find(users.begin(), users.end(), username) != users.end();
+}
+
+void CawFunction::ReadReplys(const std::string& caw_id, KeyValueInterface& kv,
+                             std::vector<Caw>& caws) {
+  Caw caw;
+  caw.ParseFromString(kv.Get("caw-" + caw_id)[0]);
+  caws.push_back(caw);
+  std::vector<std::string> children = kv.Get("cawchildren-" + caw_id);
+  for (const auto& c : children) {
+    ReadReplys(c, kv, caws);
+  }
 }
 
 }  // namespace csci499
