c434dcbbe069d137ede3844318f436ae3bfe2aff
williamsuppiger <suppiger@usc.edu>
2021-02-26 05:07:13 +0000

    added caw command line client

---
+++ src/caw/event_type.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2021, USC
+// All rights reserved.
+
+#ifndef  SRC_CAW_EVENT_TYPE_H_
+#define  SRC_CAW_EVENT_TYPE_H_
+// event type codes for Faz Server and UI to be consistent
+enum EventType {
+  kRegisteruser,
+  kCaw,
+  kFollow,
+  kRead,
+  kProfile,
+};
+
+#endif  // SRC_CAW_EVENT_TYPE_H_
